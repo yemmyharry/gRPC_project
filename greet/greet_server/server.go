1c037848a145cc5e018595211b963549c7571bef
yemmyharry <yemmyharry@gmail.com>
2022-05-04 16:22:54 +0100

    Add greet method

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"gRPC_project/greet/greetpb"
 	"google.golang.org/grpc"
 	"log"
@@ -8,6 +10,19 @@ import (
 )
 
 type server struct{}
+
+func (s server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v", req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+
+	resp := greetpb.GreetResponse{
+		Result: result,
+	}
+
+	return &resp, nil
+
+}
 
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:50051")
