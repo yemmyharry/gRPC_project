9e75705dc2d7278718ca0515d509df7dfab67396
yemmyharry <yemmyharry@gmail.com>
2022-05-05 10:08:21 +0100

    Add prime factor server streaming function

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -5,6 +5,7 @@ import (
 	"gRPC_project/calculator/calculatorpb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"io"
 	"log"
 )
 
@@ -17,7 +18,9 @@ func main() {
 	defer conn.Close()
 
 	cc := calculatorpb.NewCalculatorServiceClient(conn)
-	sumUnary(cc)
+	//sumUnary(cc)
+
+	primeFactor(cc)
 
 }
 
@@ -32,5 +35,27 @@ func sumUnary(cc calculatorpb.CalculatorServiceClient) {
 		log.Fatalf("Error while calling sum RPC %v", err)
 	}
 	log.Println("Response from Sum: ", res)
+
+}
+
+func primeFactor(cc calculatorpb.CalculatorServiceClient) {
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 12,
+	}
+
+	resStream, err := cc.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling PrimeNumberDecomposition RPC %v", err)
+	}
+	for {
+		res, err := resStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading stream %v", err)
+		}
+		log.Printf("Response from RPC: %v\n ", res)
+	}
 
 }
