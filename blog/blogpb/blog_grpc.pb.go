03f5e432db10672ecdcde72a70c0fb20d4468709
yemmyharry <yemmyharry@gmail.com>
2022-05-09 00:30:11 +0100

    Add list blogs grpc

--- blog/blogpb/blog_grpc.pb.go
+++ blog/blogpb/blog_grpc.pb.go
@@ -26,6 +26,7 @@ type BlogServiceClient interface {
 	ReadBlog(ctx context.Context, in *ReadBlogRequest, opts ...grpc.CallOption) (*ReadBlogResponse, error)
 	UpdateBlog(ctx context.Context, in *UpdateBlogRequest, opts ...grpc.CallOption) (*UpdateBlogResponse, error)
 	DeleteBlog(ctx context.Context, in *DeleteBlogRequest, opts ...grpc.CallOption) (*DeleteBlogResponse, error)
+	ListBlogs(ctx context.Context, in *ListBlogsRequest, opts ...grpc.CallOption) (BlogService_ListBlogsClient, error)
 }
 
 type blogServiceClient struct {
@@ -72,6 +73,38 @@ func (c *blogServiceClient) DeleteBlog(ctx context.Context, in *DeleteBlogReques
 	return out, nil
 }
 
+func (c *blogServiceClient) ListBlogs(ctx context.Context, in *ListBlogsRequest, opts ...grpc.CallOption) (BlogService_ListBlogsClient, error) {
+	stream, err := c.cc.NewStream(ctx, &BlogService_ServiceDesc.Streams[0], "/blog.BlogService/ListBlogs", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &blogServiceListBlogsClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type BlogService_ListBlogsClient interface {
+	Recv() (*ListBlogsResponse, error)
+	grpc.ClientStream
+}
+
+type blogServiceListBlogsClient struct {
+	grpc.ClientStream
+}
+
+func (x *blogServiceListBlogsClient) Recv() (*ListBlogsResponse, error) {
+	m := new(ListBlogsResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // BlogServiceServer is the server API for BlogService service.
 // All implementations should embed UnimplementedBlogServiceServer
 // for forward compatibility
@@ -80,6 +113,7 @@ type BlogServiceServer interface {
 	ReadBlog(context.Context, *ReadBlogRequest) (*ReadBlogResponse, error)
 	UpdateBlog(context.Context, *UpdateBlogRequest) (*UpdateBlogResponse, error)
 	DeleteBlog(context.Context, *DeleteBlogRequest) (*DeleteBlogResponse, error)
+	ListBlogs(*ListBlogsRequest, BlogService_ListBlogsServer) error
 }
 
 // UnimplementedBlogServiceServer should be embedded to have forward compatible implementations.
@@ -97,6 +131,9 @@ func (UnimplementedBlogServiceServer) UpdateBlog(context.Context, *UpdateBlogReq
 }
 func (UnimplementedBlogServiceServer) DeleteBlog(context.Context, *DeleteBlogRequest) (*DeleteBlogResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeleteBlog not implemented")
+}
+func (UnimplementedBlogServiceServer) ListBlogs(*ListBlogsRequest, BlogService_ListBlogsServer) error {
+	return status.Errorf(codes.Unimplemented, "method ListBlogs not implemented")
 }
 
 // UnsafeBlogServiceServer may be embedded to opt out of forward compatibility for this service.
@@ -182,6 +219,27 @@ func _BlogService_DeleteBlog_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _BlogService_ListBlogs_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(ListBlogsRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(BlogServiceServer).ListBlogs(m, &blogServiceListBlogsServer{stream})
+}
+
+type BlogService_ListBlogsServer interface {
+	Send(*ListBlogsResponse) error
+	grpc.ServerStream
+}
+
+type blogServiceListBlogsServer struct {
+	grpc.ServerStream
+}
+
+func (x *blogServiceListBlogsServer) Send(m *ListBlogsResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
 // BlogService_ServiceDesc is the grpc.ServiceDesc for BlogService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -206,6 +264,12 @@ var BlogService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _BlogService_DeleteBlog_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "ListBlogs",
+			Handler:       _BlogService_ListBlogs_Handler,
+			ServerStreams: true,
+		},
+	},
 	Metadata: "blog/blogpb/blog.proto",
 }
