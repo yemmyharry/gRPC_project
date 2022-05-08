43710c813cc6dc53ac9bebf5be4866c42bbf9ae6
yemmyharry <yemmyharry@gmail.com>
2022-05-08 17:15:01 +0100

    Add create blog function

---
+++ blog/blog_client/client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"gRPC_project/blog/blogpb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"log"
+)
+
+func main() {
+	dial, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("")
+	}
+
+	client := blogpb.NewBlogServiceClient(dial)
+	defer dial.Close()
+	createBlog(client)
+
+}
+
+func createBlog(c blogpb.BlogServiceClient) {
+
+	req := &blogpb.CreateBlogRequest{
+		Blog: &blogpb.Blog{
+			AuthorId: "Yemi",
+			Title:    "Fist Blog",
+			Content:  "This is my first blog",
+		},
+	}
+	blog, err := c.CreateBlog(context.Background(), req)
+	if err != nil {
+		stat, ok := status.FromError(err)
+		if ok {
+			if stat.Code() == codes.Internal {
+				log.Fatalf("Internal error: %v", stat.Message())
+			} else {
+				log.Fatalf("Unexpected error: %v", stat.Message())
+			}
+		}
+		log.Fatalf("Unexpected error: %v", err)
+	}
+
+	log.Printf("Blog created: %v", blog)
+
+}
